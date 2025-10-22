4e2fb08e3599b49fd84a7dcceafaabaec3695843
sidemix <127239280+sidemix@users.noreply.github.com>
2025-10-22 15:41:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+# main.py
+import logging
+import os
 from discord_listener import start
+
+logging.basicConfig(
+    level=os.getenv("LOG_LEVEL", "INFO"),
+    format="%(asctime)s [%(levelname)-8s] %(name)s: %(message)s",
+)
 
 if __name__ == "__main__":
     start()
