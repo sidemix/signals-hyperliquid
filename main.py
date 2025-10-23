7ff64bd4850e7c7f5c43c479961150caf24ea5c0
sidemix <127239280+sidemix@users.noreply.github.com>
2025-10-23 08:24:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 # main.py
+
 import logging
 import os
+# main.py
+from dotenv import load_dotenv
+load_dotenv()  # <-- loads .env into process env
+
 from discord_listener import start
+if __name__ == "__main__":
+    start()
+
+
 
 logging.basicConfig(
     level=os.getenv("LOG_LEVEL", "INFO"),
